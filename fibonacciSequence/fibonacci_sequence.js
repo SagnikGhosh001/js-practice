13139bb801f5e25e54327b50a643d36a606284b4
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 23:04:26 +0530

    Wrote the inner loop logic for fibonacci sequence

--- fibonacciSequence/fibonacci_sequence.js
+++ fibonacciSequence/fibonacci_sequence.js
@@ -1,6 +1,25 @@
-const rangeLimit = 3;
+const rangeLimit = 6;
 
-const firstNumber = 0;
-const secondNumber = 1;
-const total = firstNumber + secondNumber;
-console.log(firstNumber,secondNumber,total)
+let firstNumber = 0;
+let secondNumber = 1;
+let thirdNumber = firstNumber + secondNumber;
+console.log(firstNumber)
+console.log(secondNumber)
+console.log(thirdNumber)
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber)
+
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber)
+
+
+firstNumber = secondNumber;
+secondNumber = thirdNumber;
+thirdNumber = firstNumber + secondNumber;
+console.log(thirdNumber)
