46e16465418a55236eef7cf7ffae41cadb3b107c
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 14:12:10 +0530

    created a program for checking odd and even

---
+++ day2/odd-even.js
@@ -0,0 +1,12 @@
+const number = 0;
+// let isEvenOdd = "odd";
+
+//using if else
+// if (number % 2 === 0) {
+//   isEvenOdd = "even";
+// }
+
+//using ternarry
+const isEvenOdd = (number % 2 === 0) ? "even" : "odd";
+
+console.log(isEvenOdd);
