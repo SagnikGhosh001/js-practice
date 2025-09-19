0d301264e91dbab60d26a867c1214646758c53ab
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 14:11:02 +0530

    Create if-else.js

---
+++ day2/if-else.js
@@ -0,0 +1,21 @@
+
+
+//rules for if and else is
+//1.try not to use if
+//2.if you are using if try not to use else
+//3.if you use else if try not to write else
+if
+if(2 === 3) {
+  console.log("this is true");
+} else {
+  console.log("This is false");
+}
+
+
+
+if(4){
+  console.log("if");
+} else {
+  console.log("else");
+}
+
