9f9f75dda71fcb57f7adca282c877eff4cc9ffa7
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 12:45:47 +0530

    update the spelling of remainder

--- countOfDigitOfANumber/count_digit_of_number.js
+++ countOfDigitOfANumber/count_digit_of_number.js
@@ -1,14 +1,14 @@
 const input = 11;
 let copyOfInput = input;
 let countOfDigit = 0;
-let reminder = 0
+let remainder = 0
 
 while (copyOfInput !== 0) {
-  reminder = copyOfInput % 10;
+  remainder = copyOfInput % 10;
   copyOfInput = copyOfInput / 10;
 
-  if(reminder) {
-    copyOfInput = copyOfInput - (reminder/10)
+  if(remainder) {
+    copyOfInput = copyOfInput - (remainder/10)
   }
   
   countOfDigit++;
