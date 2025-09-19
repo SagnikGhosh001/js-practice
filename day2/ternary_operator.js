310228accce7061043ce03f9cd5444d5479ede0d
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 14:13:34 +0530

    Create ternary_operator.js

---
+++ day2/ternary_operator.js
@@ -0,0 +1,9 @@
+//ternary
+let age = 11;
+const msg = (age >= 18) ? "You are allowed" : "You are not allowed";
+// const isAllowed = (age >= 18) ? true : false; //we should not do it beacause the expression automaticly give you true or false
+// const isAllowed = (age >= 18) //try this one
+
+console.log(isAllowed);
+console.log(msg);
+
