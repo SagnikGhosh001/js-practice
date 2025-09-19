39a218dced63faaf1d0a010714b5c1e9e1cf6a98
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 16:13:51 +0530

    change the folder name and add operators in js file

---
+++ operator_in_js.js
@@ -0,0 +1,33 @@
+
+//Operators
+// let a = 2;
+// let b = 4;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b);
+
+//increment and decrement
+// let a = 6;
+// console.log(a++); //postfix
+// console.log(++a); //prefix
+// console.log(a);
+// console.log(a--); //postfix
+// console.log(--a); //prefix
+// console.log(a);
+
+
+
+// let a = 5;
+// // console.log(a++ + ++a);
+// console.log(a-- + --a);
+// console.log(a);
+
+
+
+// let a = 5;
+// a = ++a
+// a = a++;
+// console.log(a);
+// console.log(a);
