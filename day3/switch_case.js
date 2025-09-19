d0627325a12bd602fa88f65aa4f8f108d293509b
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-19 14:19:48 +0530

    practice odd even with switch case

---
+++ day3/switch_case.js
@@ -0,0 +1,12 @@
+//odd even using switch case
+const number = 11;
+switch(number % 2){
+  case 0:
+    console.log("Even");
+    break;
+  case 1:
+    console.log("Odd");
+    break;
+  default :
+  console.log("Something else");
+}
