101946cdb6cc29c993a6be0f6fc6ba8e0ca907e7
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-20 16:09:08 +0530

    use loop

--- vowelAndConsonantsCount/count_of_vowel_consonants_in_string.js
+++ vowelAndConsonantsCount/count_of_vowel_consonants_in_string.js
@@ -2,119 +2,28 @@ const stringToCheck = "sagnik";
 
 let vowelCounts = 0;
 let consonantsCounts = 0;
+const lengthOfString = stringToCheck.length;
 
-switch (stringToCheck[0]) {
-  case "a" :
-  case "e" :
-  case "i" :
-  case "o" :
-  case "u" :
-  case "A" :
-  case "E" :
-  case "I" :
-  case "O" :
-  case "U" :
-    vowelCounts++;
-    break;
-  case " ":
-    break;
-  default :
-    consonantsCounts++;
-}
-
-switch (stringToCheck[1]) {
-  case "a" :
-  case "e" :
-  case "i" :
-  case "o" :
-  case "u" :
-  case "A" :
-  case "E" :
-  case "I" :
-  case "O" :
-  case "U" :
-    vowelCounts++;
-    break;
-  case " ":
-    break;
-  default :
-    consonantsCounts++;
-}
-
-switch (stringToCheck[2]) {
-  case "a" :
-  case "e" :
-  case "i" :
-  case "o" :
-  case "u" :
-  case "A" :
-  case "E" :
-  case "I" :
-  case "O" :
-  case "U" :
-    vowelCounts++;
-    break;
-  case " ":
-    break;
-  default :
-    consonantsCounts++;
-}
-
-switch (stringToCheck[3]) {
-  case "a" :
-  case "e" :
-  case "i" :
-  case "o" :
-  case "u" :
-  case "A" :
-  case "E" :
-  case "I" :
-  case "O" :
-  case "U" :
-    vowelCounts++;
-    break;
-  case " ":
-    break;
-  default :
-    consonantsCounts++;
-}
-
-switch (stringToCheck[4]) {
-  case "a" :
-  case "e" :
-  case "i" :
-  case "o" :
-  case "u" :
-  case "A" :
-  case "E" :
-  case "I" :
-  case "O" :
-  case "U" :
-    vowelCounts++;
-    break;
-  case " ":
-    break;
-  default :
-    consonantsCounts++;
-}
-
-switch (stringToCheck[5]) {
-  case "a" :
-  case "e" :
-  case "i" :
-  case "o" :
-  case "u" :
-  case "A" :
-  case "E" :
-  case "I" :
-  case "O" :
-  case "U" :
-    vowelCounts++;
-    break;
-  case " ":
-    break;
-  default :
-    consonantsCounts++;
+for(let currentTerm = 0; currentTerm < lengthOfString; currentTerm++) {
+  
+  switch (stringToCheck[currentTerm]) {
+    case "a" :
+    case "e" :
+    case "i" :
+    case "o" :
+    case "u" :
+    case "A" :
+    case "E" :
+    case "I" :
+    case "O" :
+    case "U" :
+      vowelCounts++;
+      break;
+    case " ":
+      break;
+    default :
+      consonantsCounts++;
+  }
 }
 
 console.log("String:-",stringToCheck,"vowel:-",vowelCounts,"consonants:-",consonantsCounts);
