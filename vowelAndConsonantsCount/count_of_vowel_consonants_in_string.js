e5f29c37c047609fddf943972b131eaa0569f7aa
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-20 15:57:02 +0530

    Hardcoded message to see desire output

--- vowelAndConsonantsCount/count_of_vowel_consonants_in_string.js
+++ vowelAndConsonantsCount/count_of_vowel_consonants_in_string.js
@@ -1,1 +1,5 @@
-console.log("Count Vowel Consonents");
+const stringToCheck = "sagnik";
+
+const vowelCounts = 2;
+const consonantsCounts = 4;
+console.log("String:-",stringToCheck,"vowel:-",vowelCounts,"consonants:-",consonantsCounts);
