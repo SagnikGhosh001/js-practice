019542560e55f50eafea9a662bcf5ea754e7ab94
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-20 16:05:23 +0530

    write hardcoded logic of loop

--- vowelAndConsonantsCount/count_of_vowel_consonants_in_string.js
+++ vowelAndConsonantsCount/count_of_vowel_consonants_in_string.js
@@ -1,5 +1,120 @@
 const stringToCheck = "sagnik";
 
-const vowelCounts = 2;
-const consonantsCounts = 4;
+let vowelCounts = 0;
+let consonantsCounts = 0;
+
+switch (stringToCheck[0]) {
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" :
+  case "u" :
+  case "A" :
+  case "E" :
+  case "I" :
+  case "O" :
+  case "U" :
+    vowelCounts++;
+    break;
+  case " ":
+    break;
+  default :
+    consonantsCounts++;
+}
+
+switch (stringToCheck[1]) {
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" :
+  case "u" :
+  case "A" :
+  case "E" :
+  case "I" :
+  case "O" :
+  case "U" :
+    vowelCounts++;
+    break;
+  case " ":
+    break;
+  default :
+    consonantsCounts++;
+}
+
+switch (stringToCheck[2]) {
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" :
+  case "u" :
+  case "A" :
+  case "E" :
+  case "I" :
+  case "O" :
+  case "U" :
+    vowelCounts++;
+    break;
+  case " ":
+    break;
+  default :
+    consonantsCounts++;
+}
+
+switch (stringToCheck[3]) {
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" :
+  case "u" :
+  case "A" :
+  case "E" :
+  case "I" :
+  case "O" :
+  case "U" :
+    vowelCounts++;
+    break;
+  case " ":
+    break;
+  default :
+    consonantsCounts++;
+}
+
+switch (stringToCheck[4]) {
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" :
+  case "u" :
+  case "A" :
+  case "E" :
+  case "I" :
+  case "O" :
+  case "U" :
+    vowelCounts++;
+    break;
+  case " ":
+    break;
+  default :
+    consonantsCounts++;
+}
+
+switch (stringToCheck[5]) {
+  case "a" :
+  case "e" :
+  case "i" :
+  case "o" :
+  case "u" :
+  case "A" :
+  case "E" :
+  case "I" :
+  case "O" :
+  case "U" :
+    vowelCounts++;
+    break;
+  case " ":
+    break;
+  default :
+    consonantsCounts++;
+}
+
 console.log("String:-",stringToCheck,"vowel:-",vowelCounts,"consonants:-",consonantsCounts);
