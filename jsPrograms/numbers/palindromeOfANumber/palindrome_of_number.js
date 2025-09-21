937dd3672aeebf86f1f19813940c9115b60263d9
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 20:18:24 +0530

    Remove unnecessary if

--- jsPrograms/numbers/palindromeOfANumber/palindrome_of_number.js
+++ jsPrograms/numbers/palindromeOfANumber/palindrome_of_number.js
@@ -5,16 +5,13 @@ console.log("From",staringRange,"to",limit,"all palindrome numbers are:-")
 
 for (let number = staringRange; number < limit; number++){
   let inputCopy = number
-  let remainder = 0
   let reverseResult = 0
   
   while (inputCopy != 0){
+    let remainder = 0
     remainder = inputCopy % 10
     reverseResult = reverseResult * 10 + remainder
-    inputCopy = inputCopy / 10
-    if(remainder){
-      inputCopy = inputCopy - (remainder/10)
-    }  
+    inputCopy = (inputCopy - remainder) / 10
   }
   
   if(number === reverseResult){
