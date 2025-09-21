7648a412a1f8e2c9a300151d8d7190ecd5036254
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 19:37:50 +0530

    Created a program to calculate lcm for two numbers

---
+++ jsPrograms/numbers/lcm/lcm.js
@@ -0,0 +1,14 @@
+const firstNumber = 7;
+const secondNumber = 13;
+
+let gcd = 1;
+for (let divisor = 2; divisor <= firstNumber && divisor <= secondNumber; divisor++) {
+  if(firstNumber % divisor === 0 && secondNumber % divisor === 0) {
+    gcd = divisor > gcd ? divisor : gcd;
+  }
+}
+
+const lcm =(firstNumber * secondNumber) / gcd;
+
+
+console.log(lcm);
