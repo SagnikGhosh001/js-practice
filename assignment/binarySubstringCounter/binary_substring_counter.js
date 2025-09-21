fe30c6c9ab42f5a9d597ec9df2b01f3afe35b403
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 20:27:43 +0530

    removed unnecessary conditions

--- assignment/binarySubstringCounter/binary_substring_counter.js
+++ assignment/binarySubstringCounter/binary_substring_counter.js
@@ -10,10 +10,9 @@ while (decimalValueForOperation !== 0) {
   remainder = decimalValueForOperation % 2;
   binaryValue = binaryValue + remainder * binaryMultiplier;
   binaryMultiplier = binaryMultiplier * 10;
-  decimalValueForOperation = remainder ? ((decimalValueForOperation - 1) / 2) : (decimalValueForOperation / 2);
+  decimalValueForOperation = (decimalValueForOperation - remainder) / 2;
 }
 let binaryValueForOperation = binaryValue;
-
 
 const lengthOfSubString = subString.length;
 const divisor = (10 ** lengthOfSubString);
@@ -24,10 +23,10 @@ while (binaryValueForOperation !== 0) {
   remainder = binaryValueForOperation % divisor;
   matchedAnswer = ((remainder - subString) === 0) ? matchedAnswer + 1 : matchedAnswer;
   tempReminder = binaryValueForOperation % 10;
-  binaryValueForOperation = tempReminder ? ((binaryValueForOperation - 1) / 10) : (binaryValueForOperation / 10);
+  binaryValueForOperation = (binaryValueForOperation - tempReminder) / 10;
 }
 
  console.log('Number',decimalValue);
  console.log('Substring',subString);
  console.log('Binary',binaryValue);
- console.log('answear',matchedAnswer);
+ console.log('answer',matchedAnswer);
