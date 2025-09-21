57ca75feea00aef1668adc5bbf9fe488fdac2fcb
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 15:37:25 +0530

    HardCoded loop logic with if else

--- jsPrograms/numbers/largestDigitOfANumber/largest_digit_of_a_number.js
+++ jsPrograms/numbers/largestDigitOfANumber/largest_digit_of_a_number.js
@@ -1,3 +1,26 @@
-const numberToCheck = 1234;
-const largestDigit = 4;
+const numberToCheck = 6598;
+let stringFormatOfNumber = numberToCheck + "";
+let largestDigit = stringFormatOfNumber[0];
+let currentTerm = 0;
+
+if(currentTerm < stringFormatOfNumber.length) {
+  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
+  currentTerm++;
+}
+
+if(currentTerm < stringFormatOfNumber.length) {
+  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
+  currentTerm++;
+}
+
+if(currentTerm < stringFormatOfNumber.length) {
+  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
+  currentTerm++;
+}
+
+if(currentTerm < stringFormatOfNumber.length) {
+  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
+  currentTerm++;
+}
+
 console.log("Input",numberToCheck,"Max Digit",largestDigit);
