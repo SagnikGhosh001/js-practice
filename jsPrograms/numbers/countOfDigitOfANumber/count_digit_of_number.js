362f38588dc88faa520a8c9f576822e2b806fc50
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 20:15:59 +0530

    Remove unnecessary if

--- jsPrograms/numbers/countOfDigitOfANumber/count_digit_of_number.js
+++ jsPrograms/numbers/countOfDigitOfANumber/count_digit_of_number.js
@@ -1,16 +1,11 @@
-const input = 11;
+const input = 112;
 let copyOfInput = input;
 let countOfDigit = 0;
 let remainder = 0
 
 while (copyOfInput !== 0) {
   remainder = copyOfInput % 10;
-  copyOfInput = copyOfInput / 10;
-
-  if(remainder) {
-    copyOfInput = copyOfInput - (remainder/10)
-  }
-  
+  copyOfInput = (copyOfInput - remainder) / 10;  
   countOfDigit++;
 }
 console.log(countOfDigit);
