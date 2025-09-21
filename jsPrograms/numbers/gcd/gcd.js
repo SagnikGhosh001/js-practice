dc35f573d4bbc0dcc713fdb8c66e4859116e5772
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 19:34:03 +0530

    change the or operator with &&

--- jsPrograms/numbers/gcd/gcd.js
+++ jsPrograms/numbers/gcd/gcd.js
@@ -3,7 +3,7 @@ const secondNumber = 24;
 
 let gcd = 1;
 
-for (let divisor = 2; divisor <= firstNumber || divisor <= secondNumber; divisor++) {
+for (let divisor = 2; divisor <= firstNumber && divisor <= secondNumber; divisor++) {
   if(firstNumber % divisor === 0 && secondNumber % divisor === 0) {
     gcd = divisor > gcd ? divisor : gcd;
   }
