9b9d908f826bfc5949c7c129da8591026088e1a1
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 18:24:03 +0530

    Complete the code

--- jsPrograms/numbers/isANumberContainsAllUniqueNumber/is_unique_digit_of_a_number.js
+++ jsPrograms/numbers/isANumberContainsAllUniqueNumber/is_unique_digit_of_a_number.js
@@ -1,1 +1,17 @@
-console.log("are all digit of a number is unique or not");
+const numberToCheck = 76455;
+const stringFormatOfNumber = numberToCheck + "";
+
+let hasDuplicateDigit = false;
+
+for(let currentTerm = 0; currentTerm < stringFormatOfNumber.length - 1; currentTerm++) {
+  let nextTerm = currentTerm + 1;
+
+  while (nextTerm < stringFormatOfNumber.length && !hasDuplicateDigit) {
+    hasDuplicateDigit = (stringFormatOfNumber[currentTerm] === stringFormatOfNumber[nextTerm]);
+    nextTerm++;
+  }
+}
+
+const suffix = hasDuplicateDigit ? "All Digit are not Unique" : "All digits are Unique";
+
+console.log("Input",numberToCheck,"Output",suffix);
