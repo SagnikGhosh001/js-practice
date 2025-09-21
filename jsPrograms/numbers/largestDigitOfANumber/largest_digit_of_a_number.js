49937563acba033b7d2b9f30c5055d87773daf0c
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 15:39:39 +0530

    Convert if else into loop

--- jsPrograms/numbers/largestDigitOfANumber/largest_digit_of_a_number.js
+++ jsPrograms/numbers/largestDigitOfANumber/largest_digit_of_a_number.js
@@ -1,26 +1,11 @@
-const numberToCheck = 6598;
+const numberToCheck = 342;
 let stringFormatOfNumber = numberToCheck + "";
 let largestDigit = stringFormatOfNumber[0];
-let currentTerm = 0;
 
-if(currentTerm < stringFormatOfNumber.length) {
+for (let currentTerm = 0; currentTerm < stringFormatOfNumber.length; currentTerm++) {
+  
   largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
-  currentTerm++;
 }
 
-if(currentTerm < stringFormatOfNumber.length) {
-  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
-  currentTerm++;
-}
-
-if(currentTerm < stringFormatOfNumber.length) {
-  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
-  currentTerm++;
-}
-
-if(currentTerm < stringFormatOfNumber.length) {
-  largestDigit = (stringFormatOfNumber[currentTerm] > largestDigit) ? stringFormatOfNumber[currentTerm] : largestDigit
-  currentTerm++;
-}
 
 console.log("Input",numberToCheck,"Max Digit",largestDigit);
