f181004cb54c12961d21f64b402636cfc10deb26
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 11:00:46 +0530

    wrote loop logic with if else

--- jsPrograms/numbers/perfectNumber/perfect_number.js
+++ jsPrograms/numbers/perfectNumber/perfect_number.js
@@ -1,4 +1,33 @@
 const numberToCheck = 6;
 const isPerfect = true
 const suffix = isPerfect ? "Perfect Number" : "Not a Perfect Number";
+let sum = 0;
+let i = 1;
+if(numberToCheck % i === 0) {
+  sum = sum + i;
+} 
+i = i + 1;
+
+if(numberToCheck % i === 0) {
+  sum = sum + i;
+}
+i = i + 1;
+
+if(numberToCheck % i === 0) {
+  sum = sum + i;
+}
+i = i + 1;
+
+if(numberToCheck % i === 0) {
+  sum = sum + i;
+}
+i = i + 1;
+
+if(numberToCheck % i === 0) {
+  sum = sum + i;
+}
+i = i + 1;
+
+console.log(sum);
+
 console.log(numberToCheck,suffix)
