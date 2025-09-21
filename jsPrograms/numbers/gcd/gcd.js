225296b3060354c04cc34ce685c7a036844aa19b
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-21 19:17:47 +0530

    use hardcoded if else for calculating gcd

--- jsPrograms/numbers/gcd/gcd.js
+++ jsPrograms/numbers/gcd/gcd.js
@@ -1,6 +1,27 @@
 const firstNumber = 4;
 const secondNumber = 6;
 
-const gcd = 2;
+let gcd = 1;
+
+let divisor = 1;
+if(firstNumber % divisor === 0 && secondNumber % divisor === 0) {
+  gcd = divisor > gcd ? divisor : gcd;
+}
+divisor++;
+
+if(firstNumber % divisor === 0 && secondNumber % divisor === 0) {
+  gcd = divisor > gcd ? divisor : gcd;
+}
+divisor++;
+
+if(firstNumber % divisor === 0 && secondNumber % divisor === 0) {
+  gcd = divisor > gcd ? divisor : gcd;
+}
+divisor++;
+
+if(firstNumber % divisor === 0 && secondNumber % divisor === 0) {
+  gcd = divisor > gcd ? divisor : gcd;
+}
+divisor++;
 
 console.log("gcd of",firstNumber,"and",secondNumber,"is",gcd);
