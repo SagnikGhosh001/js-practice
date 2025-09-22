a34ec8fb6830b916fd0f6eff465d2bd68bf3c89d
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-22 08:32:17 +0530

    Hard Coded if else for checking harshad number

--- jsPrograms/numbers/harshadNumber/harshad_number.js
+++ jsPrograms/numbers/harshadNumber/harshad_number.js
@@ -1,4 +1,23 @@
-const input = 12;
-const isHarshd = true;
+const numberToCheck = 12;
+
+let numberForOperation = numberToCheck;
+let sumOfDigit = 0;
+let remainder = 0;
+
+if(numberForOperation !== 0) {
+  remainder = numberForOperation % 10;
+  sumOfDigit = sumOfDigit + remainder;
+  numberForOperation = (numberForOperation - remainder) / 10;
+}
+
+
+if(numberForOperation !== 0) {
+  remainder = numberForOperation % 10;
+  sumOfDigit = sumOfDigit + remainder;
+  numberForOperation = (numberForOperation - remainder) / 10;
+}
+
+let isHarshd = (numberToCheck % sumOfDigit === 0);
+
 const suffix = isHarshd ? "harshad number" : "not a harshad number";
-console.log(input,suffix)
+console.log(numberToCheck,suffix)
