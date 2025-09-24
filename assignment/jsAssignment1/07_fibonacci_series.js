df87afd62c19d6d1a3bfb706123c53d5ae9c9152
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-24 16:58:39 +0530

    Create 07_fibonacci_series.js

---
+++ assignment/jsAssignment1/07_fibonacci_series.js
@@ -0,0 +1,36 @@
+function printFibonacciSeries(rangeLimit) {
+  let currentValue = 0;
+  let nextValue = 1;
+  let fibonacciSeriesString = "";
+
+  for (let currentTerm = 1; currentTerm <= rangeLimit; currentTerm++) {
+    fibonacciSeriesString = fibonacciSeriesString + currentValue + " ";
+    const futureValue = currentValue + nextValue;
+    currentValue = nextValue;
+    nextValue = futureValue;
+  }
+  return fibonacciSeriesString;
+}
+
+function composeMessage(result, expectedResult, rangeLimit) {
+  const emoji = (result === expectedResult) ? "✅" : "❌";
+  const message = emoji + "limit of range = " + rangeLimit + " result = " + result + " expected factorial = " + expectedResult;
+  return message;
+}
+
+function testFibonacciSeries(rangeLimit, expectedResult) {
+  const result = printFibonacciSeries(rangeLimit);
+  const message = composeMessage(result, expectedResult, rangeLimit);
+  console.log(message);
+}
+
+function main() {
+  testFibonacciSeries(1, "0 ");
+  testFibonacciSeries(2, "0 1 ");
+  testFibonacciSeries(3, "0 1 1 ");
+  testFibonacciSeries(4, "0 1 1 2 ");
+  testFibonacciSeries(5, "0 1 1 2 3 ");
+  testFibonacciSeries(6, "0 1 1 2 3 5 ");
+}
+
+main();
