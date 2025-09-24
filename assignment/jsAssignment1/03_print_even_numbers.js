6aa144f661a99ec9c15f4df83c0db3c6a4cde130
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-24 15:40:50 +0530

    Create 03_print_even_numbers.js

---
+++ assignment/jsAssignment1/03_print_even_numbers.js
@@ -0,0 +1,29 @@
+function oddEvenPrintInRange(startOfTheRange, endOfTheRange) {
+  const startOfTheRangeBasedOnEvenOdd = (startOfTheRange % 2 === 0) ? startOfTheRange : startOfTheRange + 1;
+  let oddEvenSeries = "";
+
+  for (let currentNumber = startOfTheRangeBasedOnEvenOdd; currentNumber <= endOfTheRange; currentNumber = currentNumber + 2) {
+    oddEvenSeries = oddEvenSeries + currentNumber + " ";
+  }
+  return oddEvenSeries;
+}
+
+function composeMessage(result, expectedResult, startOfTheRange, endOfTheRange) {
+  const emoji = (result === expectedResult) ? "✅" : "❌";
+  const message = emoji + " start Of The Range = " + startOfTheRange + " end Of The Range = " + endOfTheRange + " result = " + result + " expected result = " + expectedResult;
+  return message;
+}
+
+function testOddEven(startOfTheRange, endOfTheRange, expectedResult) {
+  const result = oddEvenPrintInRange(startOfTheRange, endOfTheRange);
+  const message = composeMessage(result, expectedResult, startOfTheRange, endOfTheRange);
+  console.log(message);
+}
+
+function main() {
+  testOddEven(0,10,"0 2 4 6 8 10 ");
+  testOddEven(11,20,"12 14 16 18 20 ");
+  testOddEven(16,21,"16 18 20 ");
+}
+
+main();
