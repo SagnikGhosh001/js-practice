8621bd885e1f17611c94d839a5f48b0032f55095
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-24 16:14:47 +0530

    add tolerance logic

--- assignment/jsAssignment1/02_compound_interest.js
+++ assignment/jsAssignment1/02_compound_interest.js
@@ -9,11 +9,14 @@ function calculateCompoundInterest(principal, time, rate) {
   return (newPrincipal - principal);
 }
 
+function isApproximatelyEqual(result, expectedResult) {
+  const tolerence = (expectedResult - result);
+  return (tolerence <= 0.01) && (tolerence >= -0.01)
+}
+
 function composeMessage(result, expectedResult, principal, time, rate) {
-  const roundOffResult = result - (result % 1);
-  const roundOffExpectedResult = expectedResult - (expectedResult % 1);
-  const emoji = (roundOffExpectedResult === roundOffResult) ? "✅" : "❌";
-  const message = emoji + " principal = " + principal + " time = " + time + " rate of interest = " + rate + " result = " + roundOffResult + " expected result = " + roundOffExpectedResult;
+  const emoji = isApproximatelyEqual(result, expectedResult) ? "✅" : "❌";
+  const message = emoji + " principal = " + principal + " time = " + time + " rate of interest = " + rate + " result = " + result + " expected result = " + expectedResult;
   return message;
 }
 
@@ -23,12 +26,12 @@ function testCalculateCompoundInterest(principal, time, rate, expectedResult) {
   console.log(message);
 }
 
-function main (){
-  testCalculateCompoundInterest(1500,2,3,91.34);
-  testCalculateCompoundInterest(1000,2,20,440);
-  testCalculateCompoundInterest(2000,4,12,1147.03);
-  testCalculateCompoundInterest(10000,5,15,10113.36);
-  testCalculateCompoundInterest(15000,7,12,18160.22);
+function main() {
+  testCalculateCompoundInterest(1500, 2, 3, 91.34);
+  testCalculateCompoundInterest(1000, 2, 20, 440);
+  testCalculateCompoundInterest(2000, 4, 12, 1147.03);
+  testCalculateCompoundInterest(10000, 5, 15, 10113.57);
+  testCalculateCompoundInterest(15000, 7, 12, 18160.22);
 }
 
 main();
