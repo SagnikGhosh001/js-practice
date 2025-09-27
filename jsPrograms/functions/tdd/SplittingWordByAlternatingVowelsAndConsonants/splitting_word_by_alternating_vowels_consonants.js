bcbf86a686917118f67029677de5e8041902f25d
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-27 14:02:52 +0530

    able to return the first divition

--- jsPrograms/functions/tdd/SplittingWordByAlternatingVowelsAndConsonants/splitting_word_by_alternating_vowels_consonants.js
+++ jsPrograms/functions/tdd/SplittingWordByAlternatingVowelsAndConsonants/splitting_word_by_alternating_vowels_consonants.js
@@ -1,6 +1,32 @@
-function splittingWord(string) {
-  return "ape,p,l";
+function isCharacterSame(character, smallCase, upperCase) {
+  return character === smallCase || character === upperCase;
 }
+function isVowel(character) {
+  const isA = isCharacterSame(character, "a", "A");
+  const isE = isCharacterSame(character, "i", "A");
+  const isI = isCharacterSame(character, "e", "A");
+  const isO = isCharacterSame(character, "o", "A");
+  const isU = isCharacterSame(character, "u", "A");
+  return isA || isE || isI || isO || isU;
+}
+
+function extractString(string, resultString) {
+  for (let index = 1; index < string.length; index++) {
+    if (isVowel(resultString[index - 1]) !== isVowel(string[index])) {
+      resultString += string[index];
+    }
+  }
+  return resultString;
+}
+
+function splittingWord(string) {
+  let resultString = string[0];
+
+  resultString = extractString(string, resultString);
+
+  return resultString;
+}
+
 
 function symbolSelection(result, expectedResult) {
   return result === expectedResult ? "✅" : "❌";
@@ -30,7 +56,7 @@ function testSplittingWord(string, expectedResult) {
 }
 
 function main() {
-  testSplittingWord("apple","ape,p,l")
+  testSplittingWord("apple", "ape,p,l")
 }
 
 main();
