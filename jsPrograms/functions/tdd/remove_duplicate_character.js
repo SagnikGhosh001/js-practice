03a4ea5512b6bb0ddacd3b313b90fe72aceff5eb
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-27 12:17:54 +0530

    Created the structure

---
+++ jsPrograms/functions/tdd/remove_duplicate_character.js
@@ -0,0 +1,36 @@
+function removeDuplicate(string) {
+
+}
+
+function symbolSelection(result, expectedResult) {
+  return result === expectedResult ? "✅" : "❌";
+}
+
+function composeMessage(result, expectedResult, string) {
+  const symbol = symbolSelection(result, expectedResult);
+  const inputSection = "[" + result + "]";
+  const resultSection = "result = " + result;
+  const expectedResultSection = "Expected result = " + expectedResult;
+
+  let message = symbol;
+  message += "|";
+  message += inputSection;
+  message += "|";
+  message += resultSection;
+  message += "|";
+  message += expectedResultSection;
+
+  return message;
+}
+
+function testRemoveDuplicate(string, expectedResult) {
+  const result = removeDuplicate(string);
+  const message = composeMessage(result, expectedResult, string);
+  console.log(message);
+}
+
+function main() {
+
+}
+
+main();
