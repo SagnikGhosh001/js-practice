5242f341540f1822a0c496c1be59f22bdaab625e
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-09-27 21:17:20 +0530

    without using any global variables

---
+++ jsPrograms/functions/tdd/SplittingWordByAlternatingVowelsAndConsonants/splitting_word_by_alternating_vowels_and_consonants_without_using_global_variables.js
@@ -0,0 +1,79 @@
+function isVowel(character) {
+  const isA = character === "a";
+  const isE = character === "e";
+  const isI = character === "i";
+  const isO = character === "o";
+  const isU = character === "u";
+
+  return isA || isE || isI || isO || isU;
+}
+
+function splitAlternatingSegment(string, startIndex, isSegmentNeed) {
+  let segmentString = string[0];
+  let leftString = "";
+
+  for (let index = startIndex + 1; index < string.length; index++) {
+    const isSegmentStringLastCharVowel = isVowel(segmentString[segmentString.length - 1]);
+    const isMainStringLastCharVowel = isVowel(string[index]);
+
+    if (isSegmentStringLastCharVowel !== isMainStringLastCharVowel) {
+      segmentString = segmentString + string[index];
+    } else {
+      leftString = leftString + string[index];
+    }
+  }
+
+  return isSegmentNeed ? segmentString : leftString;
+}
+
+function splitWordByAlternation(string) {
+  let segmentString = "";
+  let stringForOperation = string;
+
+  while (stringForOperation !== "") {
+    segmentString = segmentString + splitAlternatingSegment(stringForOperation, 0, true) + ",";
+    stringForOperation = splitAlternatingSegment(stringForOperation, 0, false);
+  }
+
+  return segmentString;
+}
+
+function symbolSelection(result, expectedResult) {
+  return result === expectedResult ? "✅" : "❌";
+}
+
+function composeMessage(result, expectedResult, string) {
+  const symbol = symbolSelection(result, expectedResult);
+  const inputSection = "[" + string + "]";
+  const resultSection = "result = " + result;
+  const expectedResultSection = "Expected result = " + expectedResult;
+
+  let message = symbol;
+  message += "|";
+  message += inputSection;
+  message += "|";
+  message += resultSection;
+  message += "|";
+  message += expectedResultSection;
+
+  return message;
+}
+
+function testSplittingWord(string, expectedResult) {
+  const result = splittingWord(string);
+  const message = composeMessage(result, expectedResult, string);
+  console.log(message);
+}
+
+function main() {
+  testSplittingWord("apple", "ape,p,l,");
+  testSplittingWord("there", "tere,h,");
+  testSplittingWord("hello", "helo,l,");
+  testSplittingWord("abyys", "ab,y,y,s,");
+  testSplittingWord("this", "tis,h,");
+  testSplittingWord("aaabbb", "ab,ab,ab,");
+  testSplittingWord("aaaeee", "a,a,a,e,e,e,");
+  testSplittingWord("sagnik", "sagik,n,");
+}
+
+main();
