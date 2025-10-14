9d01890ef6249d3170436a67536ebbaa0c05bdff
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-10-14 12:51:37 +0530

    use bfs but not fully applicable

--- projects/auto_maze_solver.js
+++ projects/auto_maze_solver.js
@@ -1,8 +1,10 @@
 const WL = "🟫";
 const PL = "🐭";
+const VIS = "🐾";
 const ES = "  ";
 let START = [16, 1];
 const END = [17, 16];
+const USED_PATH = [];
 
 let mazeArrays = [
   [WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL, WL],
@@ -44,25 +46,44 @@ function checkIfNotWall(pos) {
   return false;
 }
 
+function checkIfPathAlreadyUsed(pos) {
+  for (let index = 0; index < USED_PATH.length; index++) {
+    const currentUsedPath = USED_PATH[index];
+    if (currentUsedPath[0] === pos[0] && currentUsedPath[1] === pos[1]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function moveAccordingResponse(x, y, addX, addY) {
   if (checkIfNotWall([x + addX, y + addY])) {
     mazeArrays[x + addX][y + addY] = PL;
-    mazeArrays[x][y] = "  ";
+    mazeArrays[x][y] = VIS;
     START = [x + addX, y + addY];
+    USED_PATH.push(START);
+  } else {
+    const randomAddX = randomNumberBetween(-1, 1);
+    const randomAddY = randomNumberBetween(-1, 1);
+    return moveAccordingResponse(x, y, randomAddX, randomAddY);
   }
 }
 
 function autoSolve(move = 0) {
+
   if (START[0] === END[0] && START[1] === END[1]) {
     console.log(move);
     return
   }
-  
-  const randomOfSetForX = randomNumberBetween(-1, 1);
-  const randomOfSetForY = randomNumberBetween(-1, 1);
-  console.log(randomOfSetForX, randomOfSetForY);
+  const dx = END[0] - START[0];
+  const dy = END[0] - START[1];
+
+  const addX = dx !== 0 ? Math.sign(dx) : 0;
+  const addY = dx !== 0 ? Math.sign(dy) : 0;
+
   console.clear();
-  moveAccordingResponse(START[0], START[1], randomOfSetForX, randomOfSetForY);
+  moveAccordingResponse(START[0], START[1], addX, addY);
   showMaze()
   return autoSolve(move + 1);
 }
