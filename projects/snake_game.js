07864ce9968c3c941f6b2055f4ecc8480f609682
SagnikGhosh001 <153536314+SagnikGhosh001@users.noreply.github.com>
2025-10-19 10:53:29 +0530

    fixed bug

--- projects/snake_game.js
+++ projects/snake_game.js
@@ -32,6 +32,14 @@ const boardArrays = [
 const MOVES = ["w", "a", "d", "s"];
 const DIRECTIONS = [[-1, 0], [0, -1], [0, 1], [1, 0]];
 
+function getHeadPosition() {
+  return body[body.length - 1];
+}
+
+function getTailPosition() {
+  return body[0];
+}
+
 function getDirection(response) {
   return DIRECTIONS[MOVES.indexOf(response)];
 }
@@ -39,6 +47,18 @@ function getDirection(response) {
 function randomNumberBetween(start, end) {
   const randomNumber = ((Math.random() * (end - start)) + start);
   return Math.round(randomNumber);
+}
+
+function ifTouchOwnBody(x, y) {
+  for (let index = 0; index < body.length; index++) {
+    const currentBody = body[index];
+
+    if (currentBody[0] === x && currentBody[1] === y) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 function setTarget() {
@@ -60,7 +80,8 @@ function showBoard() {
 
 function takingInput() {
   const response = prompt("w for up, a for left, s for down and d for right").toLowerCase().trim();
-  console.clear();
+  if (MOVES.length - MOVES.indexOf(response) === MOVES.indexOf(lastUsedMove) + 1) return takingInput();
+
   if (MOVES.includes(response)) {
     lastUsedMove = response;
     return response;
@@ -75,47 +96,29 @@ function checkIfWall(pos) {
 
 function changePos(response, point) {
   const direcion = getDirection(response);
-  const headPos = body[body.length - 1];
+  const headPos = getHeadPosition();
   moveAccordingResponse(headPos[0], headPos[1], direcion[0], direcion[1], point);
 }
 
 function addBody() {
   const directionToAdd = getDirection(lastUsedMove);
   addBodyPosition(-directionToAdd[0], -directionToAdd[1]);
-  const tail = body[0];
+  const tail = getTailPosition();
   boardArrays[tail[0]][tail[1]] = PLBODY;
 }
 
 function addBodyPosition(x, y) {
-  if (checkIfWall([body[0][0] + x, body[0][1] + y])) {
-    body.unshift([body[0][0] - x, body[0][1] - y]);
+  const tailPos = getTailPosition();
+
+  if (checkIfWall([tailPos[0] + x, tailPos[1] + y])) {
+    body.unshift([tailPos[0] - x, tailPos[1] - y]);
     return;
   }
 
   body.unshift([body[0][0] + x, body[0][1] + y]);
 }
 
-function ifTouchOwnBody(x, y) {
-  for (let index = 0; index < body.length; index++) {
-    const currentBody = body[index];
-
-    if (currentBody[0] === x && currentBody[1] === y) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function moveAccordingResponse(x, y, addX, addY, point) {
-  if (checkIfWall([x + addX, y + addY]) || ifTouchOwnBody(x + addX, y + addY)) {
-    showBoard();
-    prompt("You Loose, Your score is", point);
-    Deno.exit(0);
-  }
-
-  boardArrays[body[0][0]][body[0][1]] = ES;
-
+function movePlayerBody() {
   for (let index = 0; index < body.length - 1; index++) {
 
     const currentBody = body[index];
@@ -125,29 +128,41 @@ function moveAccordingResponse(x, y, addX, addY, point) {
     currentBody[1] = frontBody[1];
     boardArrays[currentBody[0]][currentBody[1]] = PLBODY;
   }
+}
+
+function moveAccordingResponse(x, y, addX, addY, point) {
+  if (checkIfWall([x + addX, y + addY]) || ifTouchOwnBody(x + addX, y + addY)) {
+    showBoard();
+    prompt("You Loose, Your score is", point);
+    Deno.exit(0);
+  }
+
+  const tailPos = getTailPosition();
+
+  boardArrays[tailPos[0]][tailPos[1]] = ES;
+  movePlayerBody();
 
   body[body.length - 1] = [x + addX, y + addY];
   boardArrays[x + addX][y + addY] = PL;
 }
 
 function isEaten() {
-  const headPos = body[body.length - 1];
-  return headPos[0] === TARGET[0] && headPos[1] === TARGET[1]
+  const headPos = getHeadPosition();
+  return headPos[0] === TARGET[0] && headPos[1] === TARGET[1];
 }
 
 function play(point = 0) {
   const userResponse = takingInput();
+  console.clear();
   changePos(userResponse, point);
-  showBoard();
 
   if (isEaten()) {
     addBody();
-    console.clear();
     point = point + 1;
     setTarget();
-    showBoard()
   }
 
+  showBoard();
   console.log(`score :- ${point}`);
   return play(point);
 }
