6e9939a9b38a9d2177c22bf57bdb20d96ac914ba
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2025-11-11 12:45:41 +0530

    contiguous sequence code

---
+++ jsPrograms/arrays/word_in_sequence.js
@@ -0,0 +1,54 @@
+const board = [
+  ["A", "B", "C", "E"],
+  ["S", "F", "C", "S"],
+  ["A", "D", "E", "E"],
+];
+
+const word = "ASADE";
+
+const findIndex = (acc, ele, i, char) => {
+  const index = ele.findIndex((ele) => ele === char);
+  return acc.push([i, index]) && acc;
+};
+
+const findStart = (array, word) => {
+  return array.reduce((acc, ele, i) => findIndex(acc, ele, i, word[0]), []);
+};
+
+const isBoundary = (i, j) => {
+  return i < 0 || j < 0 || i >= board.length || j >= board[i].length;
+};
+
+const traverse = (row, col, indexOfWord, index) => {
+  const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+  if (indexOfWord === word.length) return true;
+  if (directions.length === index) return false;
+
+  for (const element of directions) {
+    const newRow = row + element[0];
+    const newCol = col + element[1];
+
+    if (
+      !isBoundary(newRow, newCol) &&
+      board[newRow][newCol] === word[indexOfWord]
+    ) {
+      return traverse(newRow, newCol, indexOfWord + 1, index + 1);
+    }
+  }
+
+  return false;
+};
+
+const isSequenceAvailable = (board, word) => {
+  const startingIndexs = findStart(board, word);
+
+  for (const element of startingIndexs) {
+    if (traverse(element[0], element[1], 1, 0)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+console.log(isSequenceAvailable(board, word));
