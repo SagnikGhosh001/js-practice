916dcfaec6bba0ad3ecdb61dfb9e1a9761238cc0
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2025-11-30 23:44:18 +0530

    extract textdata outside

--- animations/marquee.js
+++ animations/marquee.js
@@ -1,19 +1,78 @@
 const board = [];
 
+const getTextData = () => [
+  {
+    pos: 0,
+    speed: 2,
+    boardPos: 0,
+    text: Deno.args[2] || "text1",
+    isVertical: false,
+  },
+  {
+    pos: board[0].length,
+    speed: 3,
+    boardPos: 2,
+    text: Deno.args[3] || "text2",
+    isVertical: false,
+  },
+  {
+    pos: 0,
+    speed: -4,
+    boardPos: 4,
+    text: Deno.args[4] || "text3",
+    isVertical: false,
+  },
+  {
+    pos: board[0].length,
+    speed: 1,
+    boardPos: 6,
+    text: Deno.args[5] || "text4",
+    isVertical: false,
+  },
+  {
+    pos: 0,
+    speed: -5,
+    boardPos: 8,
+    text: Deno.args[6] || "text5",
+    isVertical: false,
+  },
+  {
+    pos: 0,
+    speed: 1,
+    boardPos: 2,
+    text: Deno.args[7] || "text6",
+    isVertical: true,
+  },
+  {
+    pos: 0,
+    speed: -1.2,
+    boardPos: 59,
+    text: Deno.args[8] || "text7",
+    isVertical: true,
+  },
+  {
+    pos: 0,
+    speed: -0.8,
+    boardPos: 49,
+    text: Deno.args[9] || "text8",
+    isVertical: true,
+  },
+];
+
 const createBoard = (length) => {
   for (let time = 0; time < length; time++) {
     board.push(" ".repeat(+Deno.args[0] || 100).split(""));
   }
 };
 
-const drawNameOnScreenRow = (text, x, select, glitch) => {
+const drawOnScreenRow = (text, x, select, glitch) => {
   for (let index = 0; index < text.length; index++) {
     board[Math.floor(select + glitch)][(x + index) % board[select].length] =
       text[index];
   }
 };
 
-const drawNameOnScreenCol = (text, x, select, glitch) => {
+const drawOnScreenCol = (text, x, select, glitch) => {
   for (let index = 0; index < text.length; index++) {
     board[(x + index) % board.length][Math.floor(select + glitch)] =
       text[index];
@@ -40,78 +99,21 @@ const main = (args) => {
   const length = 19;
   createBoard(length);
   const speed = +args[1] * 10 || 100;
-  const textData = [
-    {
-      pos: 0,
-      speed: 2,
-      boardPos: 0,
-      text: args[2] || "text1",
-      isVertical: false,
-    },
-    {
-      pos: board[0].length,
-      speed: 3,
-      boardPos: 2,
-      text: args[3] || "text2",
-      isVertical: false,
-    },
-    {
-      pos: 0,
-      speed: -4,
-      boardPos: 4,
-      text: args[4] || "text3",
-      isVertical: false,
-    },
-    {
-      pos: board[0].length,
-      speed: 1,
-      boardPos: 6,
-      text: args[5] || "text4",
-      isVertical: false,
-    },
-    {
-      pos: 0,
-      speed: -5,
-      boardPos: 8,
-      text: args[6] || "text5",
-      isVertical: false,
-    },
-    {
-      pos: 0,
-      speed: 1,
-      boardPos: 2,
-      text: args[7] || "text6",
-      isVertical: true,
-    },
-    {
-      pos: 0,
-      speed: -1.2,
-      boardPos: 59,
-      text: args[8] || "text7",
-      isVertical: true,
-    },
-    {
-      pos: 0,
-      speed: -0.8,
-      boardPos: 49,
-      text: args[9] || "text8",
-      isVertical: true,
-    },
-  ];
+  const textData = getTextData();
 
   setInterval(() => {
     console.clear();
     const glitch = Math.random() > 0.3 ? 1.9 : 0;
+
     for (const text of textData) {
       const offset = text.speed < 0 ? board[0].length : 0;
       const moduloLength = text.isVertical ? length : board[0].length;
-      const drawFn = text.isVertical
-        ? drawNameOnScreenCol
-        : drawNameOnScreenRow;
-      text.pos = ((text.pos + Math.floor(text.speed)) + offset) %
-        moduloLength;
+      const drawFn = text.isVertical ? drawOnScreenCol : drawOnScreenRow;
+
+      text.pos = ((text.pos + Math.floor(text.speed)) + offset) % moduloLength;
       drawFn(text.text, text.pos, text.boardPos, glitch);
     }
+
     displayScreen();
     clearBoard();
   }, speed);
