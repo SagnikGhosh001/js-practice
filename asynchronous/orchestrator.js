70978f81b6f536cdba20e996a79ae3b153d63643
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-01-01 21:58:02 +0530

    implement task time for each task

--- asynchronous/orchestrator.js
+++ asynchronous/orchestrator.js
@@ -1,27 +1,40 @@
-const createTask = (name, mode) => {
+const createTask = (name, delay, mode) => {
   return new Promise((res) => {
     const startTime = Date.now();
     setTimeout(() => {
       const endTime = Date.now();
       res({ name, mode, startTime, endTime, duration: endTime - startTime });
-    }, 100);
+    }, delay);
   });
 };
 
 const orchestratorLogs = { "serial": [], "parallel": [] };
 
-const serialTaskScheduler = (task) =>
-  createTask(task, "serial").then((data) => {
+const getScheduledTime = (task) => {
+  const startIdnex = task.indexOf("[") + 1;
+  const endIdnex = task.indexOf("]");
+  return task.slice(startIdnex, endIdnex);
+};
+
+const serialTaskScheduler = (task) => {
+  const scheduledTime = getScheduledTime(task);
+
+  return createTask(task, scheduledTime, "serial").then((data) => {
     // console.log(data);
     orchestratorLogs["serial"].push(data);
   });
+};
 
-const parallelTaskScheduler = (tasks, delimeter) =>
-  Promise.all(tasks.split(delimeter).map((t) => createTask(t, "parallel")))
-    .then((data) => {
-      // console.log(data);
-      orchestratorLogs["parallel"].push(data);
-    });
+const parallelTaskScheduler = (tasks, delimeter) => {
+  const eachTasks = tasks.split(delimeter).map((t) =>
+    createTask(t, getScheduledTime(t), "parallel")
+  );
+
+  return Promise.all(eachTasks).then((data) => {
+    // console.log(data);
+    orchestratorLogs["parallel"].push(data);
+  });
+};
 
 const scheduleTask = async (tasks, delimeter) => {
   for (const task of tasks.split("\n")) {
